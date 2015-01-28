9139b33b055351bef6b63fe155b9a8bdc8c893b1
Jon Campbell <heycampbell@gmail.com>
2015-01-28 12:01:13 -0500

    updated tag validation to uniqify the names

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,7 @@
 class Tag < ActiveRecord::Base
   validates_presence_of :name
+  validates_uniqueness_of :name
+
   has_many :blogs, :through => :blog_tags
   has_many :events, :through => :event_tags
 end
