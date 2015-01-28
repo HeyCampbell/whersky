146a4382c607943a0ceb5fe7a94e2db5bd951ab9
Jon Campbell <heycampbell@gmail.com>
2015-01-28 13:18:12 -0500

    updated blog creation

--- app/controllers/blog.rb
+++ app/controllers/blog.rb
@@ -13,15 +13,17 @@ end
 
 
 post '/blog/new', auth: :author do
+  params[:blog][:user] = current_user
+ p params
   blog = Blog.create(params[:blog])
   tags = parse_tags(params[:tags])
   tags.each do |tag|
     t = Tag.find_or_create_by(name: tag)
     blog.tags << t
   end
-  blog.tags = blog.tags.uniq
+  # blog.tags = blog.tags.uniq
 
-  redirect '/blog/#{blog.id}'
+  redirect "/blog/#{blog.id}"
 end
 
 put '/blog/:id/edit', auth: :author do |id|
@@ -31,6 +33,7 @@ put '/blog/:id/edit', auth: :author do |id|
 end
 
 get '/blog/:id' do |id|
+
     erb :'blog/show', locals: {blog: Blog.find(id)}
 
 end
