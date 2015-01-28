b875fe03cec9c9a3c244cd9ab244b6122e6344df
Jon Campbell <heycampbell@gmail.com>
2015-01-28 09:23:23 -0500

    users created

--- db/migrate/20141020120711_create_users.rb
+++ db/migrate/20141020120711_create_users.rb
@@ -1,8 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :email
       t.string :name
       t.string :password_digest
+      t.string :img_url
+      t.boolean :author
+      t.boolean :admin
+      t.boolean :premium
     end
   end
 end
